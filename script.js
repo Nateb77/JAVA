90cf0e2842da4d3e9f48133d05835e3f7281e07d
Nate <126nblanco@frhsd.com>
2026-03-16 12:51:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const favoriteMovie = ["IronMan", "Avengers", "Spiderman"];
+const hobbie = new Array ("sports", "movies", "gym")
+const luckyNumbers = new Array("7", "8", "9");
+const fortune = ["yes", "no", "maybe", "ask again"]
+console.log(fortune[2])
+fortune[0] = "absoloutly";
+console.log(fortune[0]);
+fortune[4] = "awesome"
+console.log(fortune[4]);
+const superLong = ["1","2","3","4","5","6","7","8","9","10"]
+console.log(superLong.length);
+superLong[superLong.length-1] = 100000;
