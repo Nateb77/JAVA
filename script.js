a88c2ef818ac0f56ea1e1a581e6a825a2ab1bfff
Nate <126nblanco@frhsd.com>
2026-03-18 11:57:59 -0400

    !

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const favoriteMovie = ["IronMan", "Avengers", "Spiderman"];
-const hobbie = new Array ("sports", "movies", "gym")
+const hobbie = new Array ("sports", "movies", "gym");
 const luckyNumbers = new Array("7", "8", "9");
-const fortune = ["yes", "no", "maybe", "ask again"]
-console.log(fortune[2])
+const fortune = ["yes", "no", "maybe", "ask again"];
+console.log(fortune[2]);
 fortune[0] = "absoloutly";
-console.log(fortune[0]);
-fortune[4] = "awesome"
+console.log(fortune[0]);  
+fortune[4] = "awesome";
 console.log(fortune[4]);
-const superLong = ["1","2","3","4","5","6","7","8","9","10"]
+const superLong = ["1","2","3","4","5","6","7","8","9","10"];
 console.log(superLong.length);
 superLong[superLong.length-1] = 100000;
+console.log(fortune[fortune.length-1]);
+fortune.push("sometimes");
+console.log(fortune[fortune.length-1]);
+fortune.pop();
+console.log(fortune[fortune.length-1]);
+function printFirstAndThird(arr){
+    console.log(arr[0]);
+    console.log(arr[2]);
+}
